9c1ca568179fd9d205bb65a0fbdbf1085b5b5560
ryanjnam99 <ryanjnam@yahoo.com>
2023-07-02 21:05:31 -0700

    added destructuring

---
+++ script.js
@@ -0,0 +1,17 @@
+const person = {
+    firstName: 'Bob',
+    lastName: 'Marley',
+    email: 'bob@marley.com',
+    password: 'sekureP@ssw0rd9',
+    username: 'barley',
+    createdAt: 1543945177623
+};
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+
+// BEFORE ES6
+
+const { email, password } = person;
+const [firstAnimal, secondAnimal, thirdAnimal] = animals;
+console.log(email, password);
+console.log(firstAnimal, secondAnimal, thirdAnimal);
+
